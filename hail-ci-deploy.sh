7b024c711417ad706eda339215deb2bcc4030fcb
Cotton Seed <cotton@alum.mit.edu>
2018-09-10 17:21:36 -0400

    addessed comments

--- hail-ci-deploy.sh
+++ hail-ci-deploy.sh
@@ -3,7 +3,7 @@ set -ex
 SHA=$(git rev-parse --short=12 HEAD)
 
 # get sha label of batch deployment
-DEPLOYED_SHA=$(kubectl get --selector=app=batch deployments -o jsonpath='{.items[*].metadata.labels.sha}')
+DEPLOYED_SHA=$(kubectl get --selector=app=batch deployments -o jsonpath="{.items[*].metadata.labels['hail\.is/sha']}")
 
 if [[ "$SHA" == "$DEPLOYED_SHA" ]]; then
     exit 0
@@ -12,10 +12,8 @@ fi
 # requires docker
 make push-hail-ci-build-image
 
-sed -e "s,@sha,$SHA," \
+sed -e "s,@sha@,$SHA," \
     -e "s,@image@,$(cat hail-ci-build-image)," \
     < deployment.yaml.in > deployment.yaml
 
-kubectl delete -f deployment.yaml
-sleep 5
-kubectl create -f deployment.yaml
+kubectl apply -f deployment.yaml
